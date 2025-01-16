e906c1190703f5f2704b13da64038eb42dd12deb
Avi Wadhwa <aviwad@gmail.com>
2025-01-16 14:34:28 -0800

    Ran brew style --fix

--- Casks/l/lyricfever.rb
+++ Casks/l/lyricfever.rb
@@ -1,23 +1,21 @@
 cask "lyricfever" do
   version "2.0"
   sha256 "a98b7189eb8fca68069622152e11bab5e92ca4666a47443d5ad6a6db9759054a"
-  url "https://github.com/aviwad/LyricFever/releases/download/v#{version}/Lyric.Fever.#{version}.dmg", verified: "https://github.com/aviwad/LyricFever/releases/download"
+
+  url "https://github.com/aviwad/LyricFever/releases/download/v#{version}/Lyric.Fever.#{version}.dmg", verified: "github.com/aviwad/LyricFever/releases/download/"
   name "LyricsX"
   desc "Lyrics for Apple Music, Spotify with menubar, karaoke, and fullscreen support"
-  homepage "https://lyricfever.com"
+  homepage "https://lyricfever.com/"
+
+  livecheck do
+    url "https://aviwad.github.io/SpotifyLyricsInMenubar/appcast.xml"
+    strategy :sparkle
+  end
 
   auto_updates true
-
-  app "Lyric Fever.app"
-  
   depends_on macos: ">= :ventura"
 
-  zap trash: [
-    "~/Library/Containers/Lyric Fever"
-  ]
-  
-  livecheck do
-  url "https://aviwad.github.io/SpotifyLyricsInMenubar/appcast.xml"
-  strategy :sparkle
-end
+  app "Lyric Fever.app"
+
+  zap trash: "~/Library/Containers/Lyric Fever"
 end
