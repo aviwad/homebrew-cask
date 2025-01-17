818ad2da2524bc1b34682bad9cbbdbc7de588f11
Avi Wadhwa <aviwad@gmail.com>
2025-01-17 14:07:06 -0800

    Move to lyric-fever

--- Casks/l/lyric-fever.rb
+++ Casks/l/lyric-fever.rb
@@ -1,10 +1,11 @@
-cask "lyricfever" do
+cask "lyric-fever" do
   version "2.0"
   sha256 "a98b7189eb8fca68069622152e11bab5e92ca4666a47443d5ad6a6db9759054a"
 
-  url "https://github.com/aviwad/LyricFever/releases/download/v#{version}/Lyric.Fever.#{version}.dmg", verified: "github.com/aviwad/LyricFever/releases/download/"
-  name "LyricsX"
-  desc "Lyrics for Apple Music, Spotify with menubar, karaoke, and fullscreen support"
+  url "https://github.com/aviwad/LyricFever/releases/download/v#{version}/Lyric.Fever.#{version}.dmg",
+    verified: "github.com/aviwad/LyricFever/releases/download/"
+  name "Lyric Fever"
+  desc "Lyrics for Apple Music and Spotify"
   homepage "https://lyricfever.com/"
 
   livecheck do
